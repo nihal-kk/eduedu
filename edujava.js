4ac90d1712acb9ff114271b4333643a7d33569aa
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-02 18:52:50 +0530

    Add files via upload

---
+++ edujava.js
@@ -0,0 +1,4 @@
+// let fname = window.prompt("what is your fisrt name");
+// document.getElementById("fname").innerText =fname;
+// let lname = window.prompt("what is your last name");
+// document.getElementById("lname").innerText =lname;
