9ebdc1df38deb1f1618b80b9f360208b697c81d0
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-02 18:53:32 +0530

    Update edujava.js

--- edujava.js
+++ edujava.js
@@ -1,4 +1,5 @@
-// let fname = window.prompt("what is your fisrt name");
-// document.getElementById("fname").innerText =fname;
-// let lname = window.prompt("what is your last name");
-// document.getElementById("lname").innerText =lname;
+let fname = window.prompt("what is your fisrt name");
+document.getElementById("fname").innerText =fname;
+let lname = window.prompt("what is your last name");
+
+ document.getElementById("lname").innerText =lname;
