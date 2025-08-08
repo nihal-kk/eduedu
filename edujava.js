06ae50df0a90244d6e22e26bf8f61ac6765e7f30
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-08 15:36:39 +0530

    Delete edujava.js

--- edujava.js
+++
@@ -1,5 +0,0 @@
-let fname = window.prompt("what is your fisrt name");
-document.getElementById("fname").innerText =fname;
-let lname = window.prompt("what is your last name");
-
- document.getElementById("lname").innerText =lname;
